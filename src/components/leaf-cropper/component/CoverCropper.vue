b1df92d531604ec8df0e19476500472315bdd721
yaaannn <ahheyan@foxmail.com>
2025-04-10 17:54:41 +0800

    修改图片存储位置由IPFS至本地

--- src/components/leaf-cropper/component/CoverCropper.vue
+++ src/components/leaf-cropper/component/CoverCropper.vue
@@ -1,79 +1,81 @@
 <template>
-    <div class="space-cover-cropper">
-        <div class="cropper-container">
-            <pic-cropper ref="cropperRef" :key="props.file?.name" :file="props.file" :minHeight="360" :minWidth="640"
-                fillColor="#e6e6e6" @change="imgChange"></pic-cropper>
-        </div>
-        <div class="btn-container">
-            <n-button type="primary" @click="uploadPicture">裁剪并上传</n-button>
-        </div>
+  <div class="space-cover-cropper">
+    <div class="cropper-container">
+      <pic-cropper
+        ref="cropperRef"
+        :key="props.file?.name"
+        :file="props.file"
+        :minHeight="360"
+        :minWidth="640"
+        fillColor="#e6e6e6"
+        @change="imgChange"
+      ></pic-cropper>
     </div>
+    <div class="btn-container">
+      <n-button type="primary" @click="uploadPicture">裁剪并上传</n-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import PicCropper from './PicCropper.vue';
-import { NButton } from 'naive-ui';
+import { ref } from "vue";
+import PicCropper from "./PicCropper.vue";
+import { NButton } from "naive-ui";
 import { uploadFileAPI } from "@/apis/api/upload";
 
 const props = defineProps<{
-    file?: File
+  file?: File;
 }>();
 
 const cropperRef = ref<InstanceType<typeof PicCropper> | null>(null);
 
 const previewURL = ref("");
 const imgChange = (url: string) => {
-    previewURL.value = url;
-}
-const emits = defineEmits(["stateChange"])
+  previewURL.value = url;
+};
+const emits = defineEmits(["stateChange"]);
 const uploadPicture = async () => {
-    if (cropperRef.value) {
-        const file = await cropperRef.value.getFile();
-        uploadFileAPI({
-            name: "image-" + Date.now(),
-            action: "v1/public/upload/ipfs",
-            file: file,
-            onProgress: () => {
-
-            },
-            onError: () => {
-                emits("stateChange", "error");
-            },
-            onFinish: (data?: string) => {
-                emits("stateChange", "success", data);
-            },
-        }
-        )
-    }
-
-}
-
-
+  if (cropperRef.value) {
+    const file = await cropperRef.value.getFile();
+    uploadFileAPI({
+      name: "image-" + Date.now(),
+      // action: "v1/public/upload/ipfs",
+      action: "v1/public/upload",
+      file: file,
+      onProgress: () => {},
+      onError: () => {
+        emits("stateChange", "error");
+      },
+      onFinish: (data?: string) => {
+        emits("stateChange", "success", data);
+      },
+    });
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .space-cover-cropper {
-    box-sizing: border-box;
-    padding: 10px;
-    max-width: 700px;
-    max-height: 360px;
+  box-sizing: border-box;
+  padding: 10px;
+  max-width: 700px;
+  max-height: 360px;
 
-    .cropper-container {
-        padding-top: 20px;
-        width: 100%;
-        height: 280px;
+  .cropper-container {
+    padding-top: 20px;
+    width: 100%;
+    height: 280px;
+  }
+
+  .btn-container {
+    height: 60px;
+    position: relative;
+
+    button {
+      position: absolute;
+      right: 0;
+      bottom: 6px;
     }
-
-    .btn-container {
-        height: 60px;
-        position: relative;
-
-        button {
-            position: absolute;
-            right: 0;
-            bottom: 6px;
-        }
-    }
+  }
 }
 </style>
--- src/utils/resource.ts
+++ src/utils/resource.ts
@@ -1,20 +1,22 @@
-import { globalConfig } from "./global-config"
+import { globalConfig } from "./global-config";
 
 export const isURL = (url: string) => {
-    const reg = /http(s)?:\/\/((([\w-]+\.)+[\w-]+)|(localhost))(:\d+)?(\/[\w- ./?%&=#]*)?/;
-    return reg.test(url)
-}
+  const reg =
+    /http(s)?:\/\/((([\w-]+\.)+[\w-]+)|(localhost))(:\d+)?(\/[\w- ./?%&=#]*)?/;
+  return reg.test(url);
+};
 
 export const getResourceUrl = (originalUrl: string) => {
-    // 如果本身就是一个url,或者前后端同源,直接返回
-    if (isURL(originalUrl) || !globalConfig.domain) {
-        return originalUrl;
-    }
-    if (originalUrl === '') {
-        return undefined;
-    }
+  // 如果本身就是一个url,或者前后端同源,直接返回
+  if (isURL(originalUrl) || !globalConfig.domain) {
+    return originalUrl;
+  }
+  if (originalUrl === "") {
+    return undefined;
+  }
 
-    // 前后端不同源
-    // return `http${globalConfig.https ? 's' : ''}://${globalConfig.domain}${originalUrl}`;
-    return `http://${globalConfig.ipfs_domain}${originalUrl}`;
-}
+  // 前后端不同源
+  // return `http${globalConfig.https ? 's' : ''}://${globalConfig.domain}${originalUrl}`;
+  // return `http://${globalConfig.ipfs_domain}${originalUrl}`;
+  return `http://${globalConfig.domain}${originalUrl}`;
+};
